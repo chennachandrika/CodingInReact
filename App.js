1bb9ea4abe99e2132171b010a78d86844d11f074
chandrika chenna <chandrika.c@codemonk.in>
2024-11-30 13:56:53 +0530

    feat: need to keep keys as we have list of things

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1Tag" }, "hey nested one"),
-    React.createElement("h2", { id: "h1Tag" }, "hey h2 tag"),
+    React.createElement("h1", { id: "h1Tag", key: "h1" }, "hey nested one"),
+    React.createElement("h2", { id: "h1Tag", key: "h2" }, "hey h2 tag"),
   ])
 );
 const heading = React.createElement(
